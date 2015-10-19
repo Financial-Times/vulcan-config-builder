9ce251635635c11a4b4994821330e4ae391c1099
Martin Garton <martin.garton@ft.com>
2015-10-19 12:13:02 +0100

    Getting close to ready for testing.
    Still to do is removing empty dirs and probably fixing bugs.

--- all_test.go
+++ all_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/coreos/etcd/client"
+	etcderr "github.com/coreos/etcd/error"
 	"golang.org/x/net/context"
+	"reflect"
 	"testing"
 )
 
@@ -13,20 +15,20 @@ func TestReadServices(t *testing.T) {
 	}
 	kapi := client.NewKeysAPI(etcd)
 
-	if _, err := kapi.Delete(context.Background(), "/ft/services/", &client.DeleteOptions{Recursive: true}); err != nil {
-		//	t.Error(err)
+	if err := deleteRecursiveIfExists(kapi, "/vulcand/"); err != nil {
+		t.Error(err)
 	}
 
-	kvs := [][]string{
-		[]string{"/ft/services/service-a/healthcheck", "true"},
-		[]string{"/ft/services/service-a/servers/srv1", "host1:1"},
-		[]string{"/ft/services/service-a/path-prefixes", "/bananas/"},
-		[]string{"/ft/services/service-b/healthcheck", "false"},
-		[]string{"/ft/services/service-b/servers/srv1", "host1:1"},
-		[]string{"/ft/services/service-b/servers/srv2", "host2:2"},
-		[]string{"/ft/services/service-b/path-prefixes", "/content/,/bananas/"},
-	}
-	if err := setValues(kvs, kapi); err != nil {
+	if err := setValues(kapi, map[string]string{
+		"/ft/services/service-a/healthcheck":        "true",
+		"/ft/services/service-a/servers/srv1":       "http://host1:1",
+		"/ft/services/service-a/path-regex/bananas": "/bananas/.*",
+		"/ft/services/service-b/healthcheck":        "false",
+		"/ft/services/service-b/servers/srv1":       "http://host1:1",
+		"/ft/services/service-b/servers/srv2":       "http://host2:2",
+		"/ft/services/service-b/path-regex/content": "/content/.*",
+		"/ft/services/service-b/path-regex/bananas": "/bananas/.*",
+	}); err != nil {
 		t.Error(err)
 	}
 
@@ -41,60 +43,450 @@ func TestReadServices(t *testing.T) {
 	a := Service{
 		Name:           "service-a",
 		HasHealthCheck: true,
-		Addresses: []ServiceAddress{
-			ServiceAddress{Host: "host1", Port: 1},
+		Addresses:      map[string]string{"srv1": "http://host1:1"},
+		PathPrefixes: map[string]string{
+			"bananas": "/bananas/.*",
 		},
-		PathPrefixes: []string{"/bananas/"},
 	}
-	if !equalServices(a, smap["service-a"]) {
-		t.Errorf("service does not match:\n%v\n%v\n", a, smap["service-a"])
+
+	if !reflect.DeepEqual(a, smap["service-a"]) {
+		t.Errorf("service does not match. expected and acual are :\n%v\n%v\n", a, smap["service-a"])
 	}
 
 	b := Service{
 		Name:           "service-b",
 		HasHealthCheck: false,
-		Addresses: []ServiceAddress{
-			ServiceAddress{Host: "host1", Port: 1},
-			ServiceAddress{Host: "host2", Port: 2},
+		Addresses: map[string]string{
+			"srv1": "http://host1:1",
+			"srv2": "http://host2:2",
 		},
-		PathPrefixes: []string{"/content/", "/bananas/"},
+		PathPrefixes: map[string]string{
+			"bananas": "/bananas/.*",
+			"content": "/content/.*",
+		},
 	}
-	if !equalServices(b, smap["service-b"]) {
+	if !reflect.DeepEqual(b, smap["service-b"]) {
 		t.Errorf("service does not match:\n%v\n%v\n", b, smap["service-b"])
 	}
 }
 
-func equalServices(a, b Service) bool {
-	if a.HasHealthCheck != b.HasHealthCheck {
-		return false
+func TestBuildVulcanConfSingleBackend(t *testing.T) {
+	a := Service{
+		Name:           "service-a",
+		HasHealthCheck: true,
+		Addresses:      map[string]string{"srv1": "http://host1:1"},
+		PathPrefixes: map[string]string{
+			"bananas": "/bananas/.*",
+			"cheese":  "/cheese/.*",
+		},
 	}
-	if a.Name != b.Name {
-		return false
+
+	vc := buildVulcanConf([]Service{a})
+
+	expected := vulcanConf{
+		Backends: map[string]vulcanBackend{
+			"vcb-service-a": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"srv1": vulcanServer{"http://host1:1"},
+				},
+			},
+			"vcb-service-a-srv1": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"srv1": vulcanServer{"http://host1:1"},
+				},
+			},
+		},
+		FrontEnds: map[string]vulcanFrontend{
+			"vcb-byhostheader-service-a": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/.*`) && Host(`service-a`)",
+				Type:      "http",
+			},
+			"vcb-health-service-a-srv1": vulcanFrontend{
+				BackendID: "vcb-service-a-srv1",
+				Route:     "Path(`/health/service-a-srv1/__health`)",
+				Type:      "http",
+				rewrite: vulcanRewrite{
+					Id:       "rewrite",
+					Type:     "rewrite",
+					Priority: 1,
+					Middleware: vulcanRewriteMw{
+						Regexp:      "/health/service-a-srv1(.*)",
+						Replacement: "$1",
+					},
+				},
+			},
+			"vcb-service-a-path-regex-bananas": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/bananas/.*`)",
+				Type:      "http",
+			},
+			"vcb-service-a-path-regex-cheese": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/cheese/.*`)",
+				Type:      "http",
+			},
+		},
 	}
-	if len(a.Addresses) != len(b.Addresses) {
-		return false
+
+	if !reflect.DeepEqual(expected, vc) {
+		t.Errorf("vulcan conf failed. expected and actual are:\n%v\n%v\n", expected, vc)
 	}
-	for i, addr := range a.Addresses {
-		if addr != b.Addresses[i] {
-			return false
-		}
-	}
-	if len(a.PathPrefixes) != len(b.PathPrefixes) {
-		return false
-	}
-	for i, prefix := range a.PathPrefixes {
-		if prefix != b.PathPrefixes[i] {
-			return false
-		}
-	}
-	return true
+
 }
 
-func setValues(kvs [][]string, kapi client.KeysAPI) error {
-	for _, kv := range kvs {
-		if _, err := kapi.Set(context.Background(), kv[0], kv[1], &client.SetOptions{}); err != nil {
+func TestApplyVulcanConfigInitial(t *testing.T) {
+	etcd, err := client.New(client.Config{Endpoints: []string{"http://localhost:2379"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	kapi := client.NewKeysAPI(etcd)
+
+	if err := deleteRecursiveIfExists(kapi, "/vulcand/"); err != nil {
+		t.Error(err)
+	}
+
+	// should run despite no frontends/ and backends/ directories existing.
+	applyVulcanConf(kapi, vulcanConf{})
+}
+
+func TestApplyVulcanConfigRemoval(t *testing.T) {
+	etcd, err := client.New(client.Config{Endpoints: []string{"http://localhost:2379"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	kapi := client.NewKeysAPI(etcd)
+
+	if err := deleteRecursiveIfExists(kapi, "/vulcand/"); err != nil {
+		t.Error(err)
+	}
+
+	if err := setValues(kapi, map[string]string{
+		"/vulcand/backends/foo/backend":                  `{"Type": "http"}`,
+		"/vulcand/backends/foo/servers/s1":               `{"url":"http://host1.baz.com:12345"}`,
+		"/vulcand/backends/vcb-foo/backend":              `{"Type": "http"}`,
+		"/vulcand/backends/vcb-foo/servers/s1":           `{"url":"http://host1.baz.com:12345"}`,
+		"/vulcand/frontends/foo/frontend":                `{"Type":"http", "BackendId":"foo", "Route":"Path(\"foo-b\")"}`,
+		"/vulcand/frontends/foo/middlewares/rewrite":     `{"Id":"rewrite", "Type":"rewrite", "Priority":1, "Middleware": {"Regexp":"/foo/(.*)", "Replacement":"$1"}}`,
+		"/vulcand/frontends/vcb-foo/frontend":            `{"Type":"http", "BackendId":"vcb-foo", "Route":"Path(\"foo-a\")"}`,
+		"/vulcand/frontends/vcb-foo/middlewares/rewrite": `{"Id":"rewrite", "Type":"rewrite", "Priority":1, "Middleware": {"Regexp":"/foo/(.*)", "Replacement":"$1"}}`,
+	}); err != nil {
+		t.Error(err)
+	}
+
+	applyVulcanConf(kapi, vulcanConf{})
+
+	after, err := readAllKeysFromEtcd(kapi, "/vulcand/")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := map[string]string{
+		"/vulcand/backends/foo/backend":              `{"Type": "http"}`,
+		"/vulcand/backends/foo/servers/s1":           `{"url":"http://host1.baz.com:12345"}`,
+		"/vulcand/frontends/foo/frontend":            `{"Type":"http", "BackendId":"foo", "Route":"Path(\"foo-b\")"}`,
+		"/vulcand/frontends/foo/middlewares/rewrite": `{"Id":"rewrite", "Type":"rewrite", "Priority":1, "Middleware": {"Regexp":"/foo/(.*)", "Replacement":"$1"}}`,
+	}
+
+	if !reflect.DeepEqual(expected, after) {
+		t.Errorf("fail. expected and actual are \n%v\n%v\n", expected, after)
+	}
+}
+
+func TestApplyVulcanConfigCreate(t *testing.T) {
+	etcd, err := client.New(client.Config{Endpoints: []string{"http://localhost:2379"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	kapi := client.NewKeysAPI(etcd)
+
+	if err := deleteRecursiveIfExists(kapi, "/vulcand/"); err != nil {
+		t.Error(err)
+	}
+
+	vc := vulcanConf{
+		Backends: map[string]vulcanBackend{
+			"vcb-service-a": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"srv1": vulcanServer{"http://host1:1"},
+				},
+			},
+			"vcb-service-a-srv1": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"srv1": vulcanServer{"http://host1:1"},
+				},
+			},
+		},
+		FrontEnds: map[string]vulcanFrontend{
+			"vcb-byhostheader-service-a": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/.*`) && Host(`service-a`)",
+				Type:      "http",
+			},
+			"vcb-health-service-a-srv1": vulcanFrontend{
+				BackendID: "vcb-service-a-srv1",
+				Route:     "Path(`/health/service-a-srv1/__health`)",
+				Type:      "http",
+				rewrite: vulcanRewrite{
+					Id:       "rewrite",
+					Type:     "rewrite",
+					Priority: 1,
+					Middleware: vulcanRewriteMw{
+						Regexp:      "/health/service-a-srv1(.*)",
+						Replacement: "$1",
+					},
+				},
+			},
+			"vcb-service-a-path-regex-bananas": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/bananas/.*`)",
+				Type:      "http",
+			},
+			"vcb-service-a-path-regex-toast": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/toast/.*`)",
+				Type:      "http",
+			},
+		},
+	}
+
+	applyVulcanConf(kapi, vc)
+
+	values, err := readAllKeysFromEtcd(kapi, "/vulcand/")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := map[string]string{
+		"/vulcand/backends/vcb-service-a/backend":                          `{"Type":"http"}`,
+		"/vulcand/backends/vcb-service-a/servers/srv1":                     `{"url":"http://host1:1"}`,
+		"/vulcand/backends/vcb-service-a-srv1/backend":                     `{"Type":"http"}`,
+		"/vulcand/backends/vcb-service-a-srv1/servers/srv1":                `{"url":"http://host1:1"}`,
+		"/vulcand/frontends/vcb-byhostheader-service-a/frontend":           "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a\", \"Route\":\"PathRegexp(`/.*`) && Host(`service-a`)\"}",
+		"/vulcand/frontends/vcb-health-service-a-srv1/frontend":            "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a-srv1\", \"Route\":\"Path(`/health/service-a-srv1/__health`)\"}",
+		"/vulcand/frontends/vcb-health-service-a-srv1/middlewares/rewrite": `{"Id":"rewrite", "Type":"rewrite", "Priority":1, "Middleware": {"Regexp":"/health/service-a-srv1(.*)", "Replacement":"$1"}}`,
+		"/vulcand/frontends/vcb-service-a-path-regex-bananas/frontend":     "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a\", \"Route\":\"PathRegexp(`/bananas/.*`)\"}",
+		"/vulcand/frontends/vcb-service-a-path-regex-toast/frontend":       "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a\", \"Route\":\"PathRegexp(`/toast/.*`)\"}",
+	}
+
+	if !reflect.DeepEqual(expected, values) {
+		t.Errorf("fail. expected and actual are \n%v\n%v\n", expected, values)
+	}
+}
+
+// TODO: mock stuff to ensure nothing is actually updated when replacing config with itself
+func TestApplyVulcanConfigReplaceIdentical(t *testing.T) {
+	etcd, err := client.New(client.Config{Endpoints: []string{"http://localhost:2379"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	kapi := client.NewKeysAPI(etcd)
+
+	if err := deleteRecursiveIfExists(kapi, "/vulcand/"); err != nil {
+		t.Error(err)
+	}
+
+	vc := vulcanConf{
+		Backends: map[string]vulcanBackend{
+			"vcb-service-a": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"s1": vulcanServer{"http://hostz:1"},
+				},
+			},
+			"vcb-service-a-s1": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"s1": vulcanServer{"http://hostz:1"},
+				},
+			},
+		},
+		FrontEnds: map[string]vulcanFrontend{
+			"vcb-byhostheader-service-a": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/.*`) && Host(`service-a`)",
+				Type:      "http",
+			},
+			"vcb-health-service-a-s1": vulcanFrontend{
+				BackendID: "vcb-service-a-s1",
+				Route:     "Path(`/health/service-a-s1/__health`)",
+				Type:      "http",
+				rewrite: vulcanRewrite{
+					Id:       "rewrite",
+					Type:     "rewrite",
+					Priority: 1,
+					Middleware: vulcanRewriteMw{
+						Regexp:      "/health/service-a-s1(.*)",
+						Replacement: "$1",
+					},
+				},
+			},
+			"vcb-service-a-path-regex-toast1": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/toast1/.*`)",
+				Type:      "http",
+			},
+		},
+	}
+
+	applyVulcanConf(kapi, vc)
+	// replace with itself
+	println("ying")
+	applyVulcanConf(kapi, vc)
+
+	values, err := readAllKeysFromEtcd(kapi, "/vulcand/")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := map[string]string{
+		"/vulcand/backends/vcb-service-a/backend":                        `{"Type":"http"}`,
+		"/vulcand/backends/vcb-service-a/servers/s1":                     `{"url":"http://hostz:1"}`,
+		"/vulcand/backends/vcb-service-a-s1/backend":                     `{"Type":"http"}`,
+		"/vulcand/backends/vcb-service-a-s1/servers/s1":                  `{"url":"http://hostz:1"}`,
+		"/vulcand/frontends/vcb-byhostheader-service-a/frontend":         "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a\", \"Route\":\"PathRegexp(`/.*`) && Host(`service-a`)\"}",
+		"/vulcand/frontends/vcb-health-service-a-s1/frontend":            "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a-s1\", \"Route\":\"Path(`/health/service-a-s1/__health`)\"}",
+		"/vulcand/frontends/vcb-health-service-a-s1/middlewares/rewrite": `{"Id":"rewrite", "Type":"rewrite", "Priority":1, "Middleware": {"Regexp":"/health/service-a-s1(.*)", "Replacement":"$1"}}`,
+		"/vulcand/frontends/vcb-service-a-path-regex-toast1/frontend":    "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a\", \"Route\":\"PathRegexp(`/toast1/.*`)\"}",
+	}
+
+	if !reflect.DeepEqual(expected, values) {
+		t.Errorf("fail. expected and actual are \n%v\n%v\n", expected, values)
+	}
+}
+
+func TestApplyVulcanConfigReplace(t *testing.T) {
+	etcd, err := client.New(client.Config{Endpoints: []string{"http://localhost:2379"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	kapi := client.NewKeysAPI(etcd)
+
+	if err := deleteRecursiveIfExists(kapi, "/vulcand/"); err != nil {
+		t.Error(err)
+	}
+
+	applyVulcanConf(kapi, vulcanConf{
+		Backends: map[string]vulcanBackend{
+			"vcb-service-a": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"srv1": vulcanServer{"http://host1:1"},
+				},
+			},
+			"vcb-service-a-srv1": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"srv1": vulcanServer{"http://host1:1"},
+				},
+			},
+		},
+		FrontEnds: map[string]vulcanFrontend{
+			"vcb-byhostheader-service-a": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/.*`) && Host(`service-a`)",
+				Type:      "http",
+			},
+			"vcb-health-service-a-srv1": vulcanFrontend{
+				BackendID: "vcb-service-a-srv1",
+				Route:     "Path(`/health/service-a-srv1/__health`)",
+				Type:      "http",
+				rewrite: vulcanRewrite{
+					Id:       "rewrite",
+					Type:     "rewrite",
+					Priority: 1,
+					Middleware: vulcanRewriteMw{
+						Regexp:      "/health/service-a-srv1(.*)",
+						Replacement: "$1",
+					},
+				},
+			},
+			"vcb-service-a-path-regex-bananas": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/bananas/.*`)",
+				Type:      "http",
+			},
+			"vcb-service-a-path-regex-toast": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/toast/.*`)",
+				Type:      "http",
+			},
+		},
+	})
+
+	applyVulcanConf(kapi, vulcanConf{
+		Backends: map[string]vulcanBackend{
+			"vcb-service-a": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"s1": vulcanServer{"http://hostz:1"},
+				},
+			},
+			"vcb-service-a-s1": vulcanBackend{
+				Servers: map[string]vulcanServer{
+					"s1": vulcanServer{"http://hostz:1"},
+				},
+			},
+		},
+		FrontEnds: map[string]vulcanFrontend{
+			"vcb-byhostheader-service-a": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/.*`) && Host(`service-a`)",
+				Type:      "http",
+			},
+			"vcb-health-service-a-s1": vulcanFrontend{
+				BackendID: "vcb-service-a-s1",
+				Route:     "Path(`/health/service-a-s1/__health`)",
+				Type:      "http",
+				rewrite: vulcanRewrite{
+					Id:       "rewrite",
+					Type:     "rewrite",
+					Priority: 1,
+					Middleware: vulcanRewriteMw{
+						Regexp:      "/health/service-a-s1(.*)",
+						Replacement: "$1",
+					},
+				},
+			},
+			"vcb-service-a-path-regex-toast1": vulcanFrontend{
+				BackendID: "vcb-service-a",
+				Route:     "PathRegexp(`/toast1/.*`)",
+				Type:      "http",
+			},
+		},
+	})
+
+	values, err := readAllKeysFromEtcd(kapi, "/vulcand/")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := map[string]string{
+		"/vulcand/backends/vcb-service-a/backend":                        `{"Type":"http"}`,
+		"/vulcand/backends/vcb-service-a/servers/s1":                     `{"url":"http://hostz:1"}`,
+		"/vulcand/backends/vcb-service-a-s1/backend":                     `{"Type":"http"}`,
+		"/vulcand/backends/vcb-service-a-s1/servers/s1":                  `{"url":"http://hostz:1"}`,
+		"/vulcand/frontends/vcb-byhostheader-service-a/frontend":         "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a\", \"Route\":\"PathRegexp(`/.*`) && Host(`service-a`)\"}",
+		"/vulcand/frontends/vcb-health-service-a-s1/frontend":            "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a-s1\", \"Route\":\"Path(`/health/service-a-s1/__health`)\"}",
+		"/vulcand/frontends/vcb-health-service-a-s1/middlewares/rewrite": `{"Id":"rewrite", "Type":"rewrite", "Priority":1, "Middleware": {"Regexp":"/health/service-a-s1(.*)", "Replacement":"$1"}}`,
+		"/vulcand/frontends/vcb-service-a-path-regex-toast1/frontend":    "{\"Type\":\"http\", \"BackendId\":\"vcb-service-a\", \"Route\":\"PathRegexp(`/toast1/.*`)\"}",
+	}
+
+	if !reflect.DeepEqual(expected, values) {
+		t.Errorf("fail. expected and actual are \n%v\n%v\n", expected, values)
+	}
+}
+
+func setValues(kapi client.KeysAPI, kvs map[string]string) error {
+	for k, v := range kvs {
+		if _, err := kapi.Set(context.Background(), k, v, &client.SetOptions{}); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func deleteRecursiveIfExists(kapi client.KeysAPI, path string) (err error) {
+	_, err = kapi.Delete(context.Background(), path, &client.DeleteOptions{Recursive: true})
+	if e, _ := err.(client.Error); e.Code == etcderr.EcodeKeyNotFound {
+		// ignore not found.
+		err = nil
+	}
+	return err
+
 }
--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"github.com/coreos/etcd/client"
+	etcderr "github.com/coreos/etcd/error"
 	"golang.org/x/net/context"
 	"golang.org/x/net/proxy"
 	"log"
 	"net/http"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -39,7 +39,7 @@ func main() {
 
 	etcd, err := client.New(cfg)
 	if err != nil {
-		log.Fatal("failed to start etcd client: %v\n", err.Error())
+		log.Fatalf("failed to start etcd client: %v\n", err.Error())
 	}
 
 	kapi := client.NewKeysAPI(etcd)
@@ -54,24 +54,16 @@ func main() {
 
 		<-tick.C
 
-		newConf := buildVulcanConf(readServices(kapi))
-		if newConf != currentVulcanConf(kapi) {
-			setVulcanConf(newConf)
-		}
+		applyVulcanConf(kapi, buildVulcanConf(readServices(kapi)))
 	}
 
-}
-
-type ServiceAddress struct {
-	Host string
-	Port int
 }
 
 type Service struct {
 	Name           string
 	HasHealthCheck bool
-	Addresses      []ServiceAddress
-	PathPrefixes   []string
+	Addresses      map[string]string
+	PathPrefixes   map[string]string
 }
 
 func readServices(kapi client.KeysAPI) []Service {
@@ -90,28 +82,23 @@ func readServices(kapi client.KeysAPI) []Service {
 			log.Printf("skipping non-directory %v\n", node.Key)
 			continue
 		}
-		service := Service{Name: filepath.Base(node.Key)}
+		service := Service{
+			Name:         filepath.Base(node.Key),
+			Addresses:    make(map[string]string),
+			PathPrefixes: make(map[string]string),
+		}
 		for _, child := range node.Nodes {
 			switch filepath.Base(child.Key) {
 			case "healthcheck":
 				service.HasHealthCheck = child.Value == "true"
 			case "servers":
 				for _, server := range child.Nodes {
-					hostPort := strings.Split(server.Value, ":")
-					if len(hostPort) != 2 {
-						log.Printf("can't parse host and port from %v, skipping\n", server.Value)
-						continue
-					}
-					port, err := strconv.Atoi(hostPort[1])
-					if err != nil {
-						log.Printf("can't parse port from %v, skipping\n", hostPort[1])
-						continue
-					}
-					sa := ServiceAddress{Host: hostPort[0], Port: port}
-					service.Addresses = append(service.Addresses, sa)
+					service.Addresses[filepath.Base(server.Key)] = server.Value
 				}
-			case "path-prefixes":
-				service.PathPrefixes = strings.Split(child.Value, ",")
+			case "path-regex":
+				for _, path := range child.Nodes {
+					service.PathPrefixes[filepath.Base(path.Key)] = path.Value
+				}
 			default:
 				fmt.Printf("skipped key %v for node %v\n", child.Key, child)
 			}
@@ -122,22 +109,205 @@ func readServices(kapi client.KeysAPI) []Service {
 }
 
 type vulcanConf struct {
-	// TODO
+	FrontEnds map[string]vulcanFrontend
+	Backends  map[string]vulcanBackend
+}
+
+type vulcanFrontend struct {
+	BackendID string
+	Route     string
+	Type      string
+	rewrite   vulcanRewrite
+}
+
+type vulcanRewrite struct {
+	Id         string
+	Type       string
+	Priority   int
+	Middleware vulcanRewriteMw
+}
+
+type vulcanRewriteMw struct {
+	Regexp      string
+	Replacement string
+}
+
+type vulcanBackend struct {
+	Servers map[string]vulcanServer
+}
+
+type vulcanServer struct {
+	URL string
 }
 
 func buildVulcanConf(services []Service) vulcanConf {
-	fmt.Printf("services: %v\n", services)
-	println("TODO: IMPLEMENT ME")
-	return vulcanConf{}
+	vc := vulcanConf{
+		Backends:  make(map[string]vulcanBackend),
+		FrontEnds: make(map[string]vulcanFrontend),
+	}
+
+	for _, service := range services {
+
+		// "main" backend
+		mainBackend := vulcanBackend{Servers: make(map[string]vulcanServer)}
+		backendName := fmt.Sprintf("vcb-%s", service.Name)
+		for svrID, sa := range service.Addresses {
+			mainBackend.Servers[svrID] = vulcanServer{sa}
+		}
+		vc.Backends[backendName] = mainBackend
+
+		// Host header front end
+		frontEndName := fmt.Sprintf("vcb-byhostheader-%s", service.Name)
+		vc.FrontEnds[frontEndName] = vulcanFrontend{
+			Type:      "http",
+			BackendID: backendName,
+			Route:     fmt.Sprintf("PathRegexp(`/.*`) && Host(`%s`)", service.Name),
+		}
+
+		// instance backends & health front ends
+		for svrID, sa := range service.Addresses {
+			instanceBackend := vulcanBackend{Servers: make(map[string]vulcanServer)}
+			instanceBackend.Servers[svrID] = vulcanServer{sa}
+			backendName := fmt.Sprintf("vcb-%s-%s", service.Name, svrID)
+			vc.Backends[backendName] = instanceBackend
+
+			frontEndName := fmt.Sprintf("vcb-health-%s-%s", service.Name, svrID)
+
+			vc.FrontEnds[frontEndName] = vulcanFrontend{
+				Type:      "http",
+				BackendID: backendName,
+				Route:     fmt.Sprintf("Path(`/health/%s-%s/__health`)", service.Name, svrID),
+				rewrite: vulcanRewrite{
+					Id:       "rewrite",
+					Type:     "rewrite",
+					Priority: 1,
+					Middleware: vulcanRewriteMw{
+						Regexp:      fmt.Sprintf("/health/%s-%s(.*)", service.Name, svrID),
+						Replacement: "$1",
+					},
+				},
+			}
+
+		}
+
+		// public path front ends
+		for pathName, pathRegex := range service.PathPrefixes {
+			vc.FrontEnds[fmt.Sprintf("vcb-%s-path-regex-%s", service.Name, pathName)] = vulcanFrontend{
+				Type:      "http",
+				BackendID: backendName,
+				Route:     fmt.Sprintf("PathRegexp(`%s`)", pathRegex),
+			}
+		}
+	}
+
+	return vc
 }
 
-func currentVulcanConf(kapi client.KeysAPI) vulcanConf {
-	println("TODO: IMPLEMENT ME")
-	return vulcanConf{}
+func applyVulcanConf(kapi client.KeysAPI, vc vulcanConf) {
+
+	newConf := vulcanConfToEtcdKeys(vc)
+
+	existing, err := readAllKeysFromEtcd(kapi, "/vulcand/")
+	if err != nil {
+		panic(err)
+	}
+
+	for k, v := range existing {
+		// keep the keys not created by us
+		if !strings.HasPrefix(k, "/vulcand/backends/vcb-") && !strings.HasPrefix(k, "/vulcand/frontends/vcb-") {
+			newConf[k] = v
+		}
+	}
+
+	// remove unwanted frontends
+	for k, _ := range existing {
+		if strings.HasPrefix(k, "/vulcand/frontends/vcb-") {
+			_, found := newConf[k]
+			if !found {
+				log.Printf("deleting frontend %s\n", k)
+				_, err := kapi.Delete(context.Background(), k, &client.DeleteOptions{Recursive: false})
+				if err != nil {
+					log.Printf("error deleting frontend %v\n", k)
+				}
+			}
+		}
+	}
+
+	// remove unwanted backends
+	for k, _ := range existing {
+		if strings.HasPrefix(k, "/vulcand/backends/vcb-") {
+			_, found := newConf[k]
+			if !found {
+				log.Printf("deleting backend%s\n", k)
+				_, err := kapi.Delete(context.Background(), k, &client.DeleteOptions{Recursive: false})
+				if err != nil {
+					log.Printf("error deleting backend %v\n", k)
+				}
+			}
+		}
+	}
+
+	// add or modify backends
+	for k, v := range newConf {
+		oldVal := existing[k]
+		if v != oldVal {
+			log.Printf("setting backend %s to %s\n", k, v)
+			if _, err := kapi.Set(context.Background(), k, v, nil); err != nil {
+				log.Printf("error setting %s to %s\n", k, v)
+			}
+		}
+	}
+
+	// add or modify frontends
+	for k, v := range newConf {
+		oldVal := existing[k]
+		if v != oldVal {
+			log.Printf("setting frontend %s to %s\n", k, v)
+			if _, err := kapi.Set(context.Background(), k, v, nil); err != nil {
+				log.Printf("error setting %s to %s\n", k, v)
+			}
+		}
+	}
 }
 
-func setVulcanConf(vc vulcanConf) {
-	println("TODO: IMPLEMENT ME")
+func vulcanConfToEtcdKeys(vc vulcanConf) map[string]string {
+	m := make(map[string]string)
+
+	// create backends
+	for beName, be := range vc.Backends {
+		k := fmt.Sprintf("/vulcand/backends/%s/backend", beName)
+		v := `{"Type":"http"}`
+		m[k] = v
+
+		for sName, s := range be.Servers {
+			k := fmt.Sprintf("/vulcand/backends/%s/servers/%s", beName, sName)
+			v := fmt.Sprintf(`{"url":"%s"}`, s.URL)
+			m[k] = v
+		}
+
+	}
+
+	// create frontends
+	for feName, be := range vc.FrontEnds {
+		k := fmt.Sprintf("/vulcand/frontends/%s/frontend", feName)
+		v := fmt.Sprintf(`{"Type":"%s", "BackendId":"%s", "Route":"%s"}`, be.Type, be.BackendID, be.Route)
+		m[k] = v
+		if be.rewrite.Id != "" {
+			k := fmt.Sprintf("/vulcand/frontends/%s/middlewares/rewrite", feName)
+			v := fmt.Sprintf(
+
+				`{"Id":"%s", "Type":"%s", "Priority":%d, "Middleware": {"Regexp":"%s", "Replacement":"%s"}}`,
+				be.rewrite.Id,
+				be.rewrite.Type,
+				be.rewrite.Priority,
+				be.rewrite.Middleware.Regexp,
+				be.rewrite.Middleware.Replacement,
+			)
+			m[k] = v
+		}
+	}
+
+	return m
 }
 
 func newWatcher(kapi client.KeysAPI, path string, socksProxy string, etcdPeers []string) watcher {
@@ -170,4 +340,28 @@ type watcher struct {
 
 func (w *watcher) wait() <-chan struct{} {
 	return w.ch
+}
+
+func readAllKeysFromEtcd(kapi client.KeysAPI, root string) (map[string]string, error) {
+	m := make(map[string]string)
+
+	resp, err := kapi.Get(context.Background(), root, &client.GetOptions{Recursive: true})
+	if err != nil {
+		if e, _ := err.(client.Error); e.Code == etcderr.EcodeKeyNotFound {
+			return m, nil
+		}
+		panic(err)
+	}
+	addAllValuesToMap(m, resp.Node)
+	return m, nil
+}
+
+func addAllValuesToMap(m map[string]string, node *client.Node) {
+	if node.Dir {
+		for _, child := range node.Nodes {
+			addAllValuesToMap(m, child)
+		}
+	} else {
+		m[node.Key] = node.Value
+	}
 }
